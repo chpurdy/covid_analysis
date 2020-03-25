c1b294be901b17424564cad1f47e8b60162de8a3
Chad Purdy <chpurdy@gmail.com>
2020-03-24 22:50:31 -0500

    add update script

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -o time_series_covid19_confirmed_global.csv
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -o time_series_covid19_deaths_global.csv
